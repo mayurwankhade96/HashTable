9caddf9c6c024014d947378392557ef5e4d843f8
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-23 13:55:42 +0530

    [Mayur] Add : Welcome message added.

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Hash Table Program!");
+        }
+    }
+}
