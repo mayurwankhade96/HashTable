8d6a2ba27c7fa9cab1fd3528378a53af9ea12d71
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-06-25 16:11:49 +0530

    [Mayur] Refactor : Removed avoidable word.

--- HashTable/HashTable/MyMapNode.cs
+++ HashTable/HashTable/MyMapNode.cs
@@ -14,12 +14,22 @@ namespace HashTable
             this.items = new LinkedList<KeyValue<K, V>>[size];
         }
 
+        /// <summary>
+        /// Method to get position of the key in the array using GetHashCode() method
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public int GetArrayPosition(K key)
         {
             int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
+        /// <summary>
+        /// Method to get value based on its key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public V Get(K key)
         {
             int position = GetArrayPosition(key);
@@ -34,6 +44,11 @@ namespace HashTable
             return default(V);
         }
 
+        /// <summary>
+        /// Method to add KeyValue pair to the linked list of type KeyValue<K, V>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         public void Add(K key, V value)
         {
             int position = GetArrayPosition(key);
@@ -42,6 +57,11 @@ namespace HashTable
             linkedList.AddLast(item);
         }
 
+        /// <summary>
+        /// Method to get linked list based on its position
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
         public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
         {
             LinkedList<KeyValue<K, V>> linkedList = items[position];
@@ -53,6 +73,36 @@ namespace HashTable
             return linkedList;
         }
 
+        /// <summary>
+        /// Method to remove word based on given key
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+                if (itemFound)
+                {
+                    Console.WriteLine("Item Removed " + foundItem.key);
+                    linkedList.Remove(foundItem);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A structure that has key and value
+        /// </summary>
+        /// <typeparam name="K"></typeparam>
+        /// <typeparam name="V"></typeparam>
         public struct KeyValue<K, V>
         {
             public K key { get; set; }
